7119a5052db55cf14fef515ab8c21aca48498394
Kiran Navale <34519689+kirannavale@users.noreply.github.com>
2023-03-31 13:07:00 +0530

    Add files via upload

--- DDL/DDL.sql
+++ DDL/DDL.sql
@@ -1,7 +1,11 @@
 
- /*DDL (Data Definition Language)
+ --DDL (Data Definition Language)--
+
+--CREATE DATABASE
 
 CREATE DATABASE sql; 
+
+--CREATE TABLE
 
 CREATE TABLE customers (
   customer_id INT PRIMARY KEY,
@@ -11,85 +15,117 @@ CREATE TABLE customers (
   address_id INT
 );
 
-INSERT INTO customers (customer_id, first_name, last_name, email, address_id) VALUES
-(1, 'Mary', 'Smith', 'mary.smith@mailid.com', 5),
-(2, 'Patricia', 'Johnson', 'patricia.johnson@mailid.com', 6),
-(3, 'Madan', 'Mohan', 'madan.mohan@mailid.com', 7),
-(4, 'Barbara', 'Jones', 'barbara.jones@mailid.com', 8),
-(5, 'Elizabeth', 'Brown', 'elizabeth.brown@mailid.com', 9),
-(6, 'Jennifer', 'Davis', 'jennifer.davis@mailid.com', 10),
-(7, 'Maria', 'Miller', 'maria.miller@mailid.com', 11),
-(8, 'Susan', 'Wilson', 'susan.wilson@mailid.com', 12),
-(9, 'R', 'Madhav', 'r.madhav@mailid.com', 13),
-(10, 'Dorothy', 'Taylor', 'dorothy.taylor@mailid.com', 14),
-(11, 'Lisa', 'Anderson', 'lisa.anderson@mailid.com', 15),
-(12, 'Nancy', 'Thomas', 'nancy.thomas@mailid.com', 16),
-(13, 'Karen', 'Jackson', 'karen.jackson@mailid.com', 17),
-(14, 'Betty', 'White', 'betty.white@mailid.com', 18),
-(15, 'Helen', 'Harris', 'helen.harris@mailid.com', 19); 
+--CREATE VIEW
 
 CREATE VIEW customer_view AS
 SELECT customer_id, first_name, last_name, email, address_id
 FROM customers; 
 
- ALTER
+ --ALTER
+
 --Add column
 ALTER TABLE customers
 ADD COLUMN countries VARCHAR(50);
 
---CONSTRAINTS
-
---PRIMARY KEY
-ALTER TABLE customers
-ADD PRIMARY KEY (customer_id); 
-
---NOT NULL
-ALTER TABLE customers
-ALTER COLUMN first_name SET NOT NULL;
+--CONSTRAINTS--
 
 --UNIQUE
-ALTER TABLE customers
-ADD CONSTRAINT unique_countries UNIQUE (countries);
+CREATE TABLE customers 
+(
+  customer_id INT NOT NULL UNIQUE
+  first_name VARCHAR(50),
+  last_name VARCHAR(50),
+  email NOT NULL
+ );
 
---FOREIGN KEY
-ALTER TABLE customers
-ADD CONSTRAINT fk_address_id
-FOREIGN KEY (address_id)
-REFERENCES addresses(address_id);
+ --NOT NULL
+
+CREATE TABLE customers 
+(
+  customer_id NOT NULL
+  first_name VARCHAR(50),
+  last_name VARCHAR(50),
+  email NOT NULL
+ );
+
+--PRIMARY KEY
+
+CREATE TABLE customers
+(
+  customer_id INT NOT NULL,
+  first_name VARCHAR(50),
+  last_name VARCHAR(50),
+  email VARCHAR(255) NOT NULL,
+  PRIMARY KEY (customer_id)
+);
+
+ --FOREIGN KEY
+
+ CREATE TABLE Employees
+(
+Employee_ID int NOT NULL,
+Name VARCHAR(50),
+Department_ID int NOT NULL,
+PRIMARY KEY (Employee_ID),
+FOREIGN KEY (Department_ID ) REFERENCES
+Departments (Department_ID)
+);
+
 
 --CHECK
-ALTER TABLE customers
-ADD CONSTRAINT check_email
-CHECK (email LIKE '%@mailid.com');
+
+CREATE TABLE customers 
+(
+  customer_id INT PRIMARY KEY,
+  first_name VARCHAR(50),
+  last_name VARCHAR(50),
+  email VARCHAR(100) CHECK (email LIKE '%@mailid.com'),
+  address_id INT
+  );
 
 --DEFAULT
-ALTER TABLE customers
-ALTER COLUMN address_id SET DEFAULT 1;
 
---REMOVE 
+CREATE TABLE customers 
+(
+  customer_id INT PRIMARY KEY,
+  first_name VARCHAR(50),
+  last_name VARCHAR(50),
+  email VARCHAR(100),
+  address_id INT DEFAULT 1
+  );
+
+
+--REMOVE COLUMN
+
 ALTER TABLE customers
 DROP COLUMN countries;
 
---CONSTRAINT
+--REMOVE CONSTRAINT
+
 ALTER TABLE customers
 DROP CONSTRAINT check_email;
 
 --MODIFY DATATYPE
+
 ALTER TABLE table_name
 ALTER COLUMN first_name NVARCHAR(50);
 
 --MODIFY DATATYPE LENGTH
+
 ALTER TABLE table_name
-ALTER COLUMN first_name VARCHAR(100);
+ALTER COLUMN first_name NVARCHAR(100);
 
 --DROP
+
 DROP TABLE customers;
 
 --TRUNCATE
+
 TRUNCATE TABLE customers; 
 
 --DROP DATABASE
-DROP DATABASE sql;*/
+
+DROP DATABASE sql;
 
 
 
